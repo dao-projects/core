77e853d2635465ae350476c7ead20b5b7f9cf26f
daoxin <liuhangbiaoo@gmail.comm>
2023-08-14 10:46:48 +0800

    类型转小写

--- packages/getclass/index.js
+++ packages/getclass/index.js
@@ -1,4 +1,20 @@
 
+/**
+ * Gets the class.
+ *
+ * @param      {<type>}  object  The object
+ * @return     {<type>}  The class.
+ * getClass(5); // => "Number"
+ * getClass({}); // => "Object"
+ * getClass(/foo/); // => "RegExp"
+ * getClass(''); // => "String"
+ * getClass(true); // => "Boolean"
+ * getClass([]); // => "Array"
+ * getClass(undefined); // => "Window"
+ * getClass(Element); // => "Constructor"
+ * getClass(()=>); // => "Function"
+ */
+
 export const getClass = function(object){
-    return Object.prototype.toString.call(object).match(/^\[object\s(.*)\]$/)[1];
+    return Object.prototype.toString.call(object).match(/^\[object\s(.*)\]$/)[1]?.toLowerCase();
 }
